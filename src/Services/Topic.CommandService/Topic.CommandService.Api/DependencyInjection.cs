54379ece9033f2eb8ea68c0638d40ae5d03598c2
Vladislav Mikkoev <vladislav.mikkoev@yandex.ru>
2025-07-06 06:14:15 +0300

    feat<DependencyInjection>: Зарегистрировать обработчики команд
    На комментарии и топики

--- src/Services/Topic.CommandService/Topic.CommandService.Api/DependencyInjection.cs
+++ src/Services/Topic.CommandService/Topic.CommandService.Api/DependencyInjection.cs
@@ -1,9 +1,21 @@
 using Core.Events.Dao;
 using Core.Handlers;
+using Core.MediatR;
 using Core.Services;
 using Marten;
+using Topic.CommandService.Api.Comments;
+using Topic.CommandService.Api.Comments.Commands.CreateComment;
+using Topic.CommandService.Api.Comments.Commands.RemoveComment;
+using Topic.CommandService.Api.Comments.Commands.UpdateComment;
+using Topic.CommandService.Api.Topic;
+using Topic.CommandService.Api.Topic.Commands;
+using Topic.CommandService.Api.Topic.Commands.CreateTopic;
+using Topic.CommandService.Api.Topic.Commands.LikeTopic;
+using Topic.CommandService.Api.Topic.Commands.RemoveTopic;
+using Topic.CommandService.Api.Topic.Commands.UpdateTopic;
 using Topic.CommandService.Domain.Aggregates;
 using Topic.CommandService.Infrastructure.Handlers;
+using Topic.CommandService.Infrastructure.MediatR;
 using Topic.CommandService.Infrastructure.Services;
 
 namespace Topic.CommandService.Api;
@@ -23,7 +35,34 @@ public static class DependencyInjection
         services.AddScoped<IEventStorage, EventStorage>();
         services.AddScoped<IEventService, EventService>();
         services.AddScoped<IEventHandler<ContentAggregate>, EventHandlerImpl>();
+        services.AddScoped<ICommentCommandHandler, CommentCommandHandler>();
+        services.AddScoped<ITopicCommandHandler, TopicCommandHandler>();
+        services.RegisterCommandHandler();
         
+        return services;
+    }
+
+    private static IServiceCollection RegisterCommandHandler(
+        this IServiceCollection services)
+    {
+        services.AddScoped<ICommandDispatcher>(provider =>
+        {
+            var dispatcher = new CommandDispatcher();
+            
+            var commandCommentHandler = provider.GetRequiredService<ICommentCommandHandler>();
+            dispatcher.RegisterHandler<CreateCommentCommand>(command => commandCommentHandler.HandleAsync(command, CancellationToken.None));
+            dispatcher.RegisterHandler<UpdateCommentCommand>(command => commandCommentHandler.HandleAsync(command, CancellationToken.None));
+            dispatcher.RegisterHandler<RemoveCommentCommand>(command => commandCommentHandler.HandleAsync(command, CancellationToken.None));
+            
+            var commandTopicHandler = provider.GetRequiredService<ITopicCommandHandler>();
+            dispatcher.RegisterHandler<CreateTopicCommand>(command => commandTopicHandler.HandleAsync(command, CancellationToken.None));
+            dispatcher.RegisterHandler<RemoveTopicCommand>(command => commandTopicHandler.HandleAsync(command, CancellationToken.None));
+            dispatcher.RegisterHandler<UpdateTopicCommand>(command => commandTopicHandler.HandleAsync(command, CancellationToken.None));
+            dispatcher.RegisterHandler<LikeTopicCommand>(command => commandTopicHandler.HandleAsync(command, CancellationToken.None));
+
+            return dispatcher;
+        });
+
         return services;
     }
 }
