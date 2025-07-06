ee7067767a18d67de4706d4b3691932676665771
Vladislav Mikkoev <vladislav.mikkoev@yandex.ru>
2025-07-06 18:37:43 +0300

    feat<BaseEndpoint>: Добавить основу для эндпоинтов

---
+++ src/Services/Topic.CommandService/Topic.CommandService.Api/ResponseDtos/ResponseDto.cs
@@ -0,0 +1,8 @@
+using Core.ResponseDtos;
+
+namespace Topic.CommandService.Api.ResponseDtos;
+
+public class ResponseDto: BaseResponse
+{
+    
+}
---
+++ src/Shared/Core/ResponseDtos/BaseResponse.cs
@@ -0,0 +1,6 @@
+namespace Core.ResponseDtos;
+
+public class BaseResponse
+{
+    public required string Message { get; set; }
+}
