79971b3d85b9fadf83a9739251422e51842fb4d6
Vladislav Mikkoev <vladislav.mikkoev@yandex.ru>
2025-07-13 08:38:23 +0300

    feat: Настроить лимит запросов для всех клиентов

--- src/ApiGateways/YarpApi/Program.cs
+++ src/ApiGateways/YarpApi/Program.cs
@@ -1,10 +1,24 @@
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("YarpProxy"));
 
+builder.Services.AddRateLimiter(option =>
+{
+    option.AddFixedWindowLimiter("fixed", config =>
+    {
+        config.PermitLimit = 3;
+        config.Window = TimeSpan.FromSeconds(30);
+    });
+});
+
 var app = builder.Build();
 
-app.MapReverseProxy();
+app.UseRateLimiter();
+app.MapReverseProxy()
+    .RequireRateLimiting("fixed");
 
 await app.RunAsync();
